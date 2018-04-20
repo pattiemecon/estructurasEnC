27032c3791a57d31447611898f488618012f5c89
pepe <zz@zz.com>
2018-04-20 17:31:13 -0300

    Primera estructura

---
+++ EjemploEstructuras/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int legajo;
+    int notaUno;
+    int notaDos;
+    float promedio;
+    char nombre[50];
+    int estado;
+} sAlumno;
+
+int main()
+{
+
+    return 0;
+}
